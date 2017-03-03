6adb879c4b1acf59e5634871d8a3720b2ffbb3b9
netpi <moon@netpi.me>
2017-03-03 23:00:24 +0800

    1.0.0-beta init

---
+++ README.md
@@ -0,0 +1,39 @@
+# Pagination
+
+Pagination component for Vue Bulma.
+
+## Installation
+
+```sh
+$ npm install vue-bulma-pagination --save
+or
+$ yarn add vue-bulma-pagination --save
+```
+
+## Examples
+
+```vue
+<template>
+  <div>
+    <pagination :urlPrefix='/' :currentPage=2 :pageLength=5 /> 
+  </div>
+</template>
+
+<script>
+import Pagination from 'vue-bulma-pagination'
+
+export default {
+  components: {
+    Pagination
+  }
+}
+</script>
+```
+
+
+## Badges
+
+![](https://img.shields.io/badge/license-MIT-blue.svg)
+![](https://img.shields.io/badge/status-dev-yellow.svg)
+
+---
---
+++ src/Pagination.vue
@@ -0,0 +1,60 @@
+<template>
+    <nav class="pagination">
+        <router-link :class="getPreClassName()" :to="urlPrefix+'/'+(formatCurrentPage-1)" >Prev</router-link>
+        <router-link :class="getNextClassName()" :to="+urlPrefix+'/'+(formatCurrentPage+1)">Next</router-link>
+        <ul class="pagination-list" >
+            <li v-for="item in pagingList" >
+            <router-link v-if="item !== '...'" :class=" getPagingClassName(item) " :to="'/'+urlPrefix+'/'+item">{{item}}</router-link>
+            <span v-else class="pagination-ellipsis">...</span>
+            </li>                
+        </ul>
+    </nav>  
+</template>
+<script>
+import paging from './paging.js'
+export default {
+  name: 'vue-bulma-pagination',
+  props: {
+    currentPage: {
+      type: Number,
+      default: 1
+    },
+    pageLength: Number,
+    urlPrefix: {
+        type:String,
+        default:'/'
+    },
+    displayLength: {
+        type: Number,
+        default: 4
+    }
+  },
+  computed: {
+    pagingList () {
+      return paging(this.currentPage, this.pageLength, 4)
+    },
+    formatCurrentPage () {
+      const currentPage = Number(this.currentPage)
+      return currentPage > 0 ? currentPage : 1
+    },
+    getPagingClassName (item) {
+      return this.currentPage !== item ? 'pagination-link' : 'pagination-link is-current'
+    },
+    getPreClassName () {
+      return this.currentPage !== 1 ? 'pagination-previous' : 'pagination-previous is-disabled'
+    },
+    getNextClassName () {
+      return this.currentPage < this.pageLength ? 'pagination-next' : 'pagination-next is-disabled'
+    }
+  }
+}
+</script>
+
+<style >
+.pagination-list {
+    list-style : none ;    
+}
+.pagination-list li {
+    list-style : none ;
+}
+</style>
